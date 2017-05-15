d07e15906a7432de6e55c609d03299e11dcd9d11
Yunjong Jeong <yunjong@kaist.ac.kr>
2017-05-15 18:38:27 +0900

    Add minimal board list page

--- web/bbs/views.py
+++ web/bbs/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from .models import Post
 
 def index(request):
     return render(request, 'bbs/index.html', {})
+
+def list(request):
+    posts = Post.objects.all()
+    return render(request, 'bbs/list.html', {'posts': posts})
