a64ba18dc3c759ada2b928f01207ca2da8a30389
Yunjong Jeong <yunjong@kaist.ac.kr>
2017-05-16 16:16:53 +0900

    Implement write post menu without user auth

--- web/bbs/views.py
+++ web/bbs/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from .models import Post
+from django.shortcuts import render, redirect
+from .models import Post, BoardUser
 
 def index(request):
     return render(request, 'bbs/index.html', {})
@@ -12,4 +12,11 @@ def write(request):
     if request.method == 'GET':
         return render(request, 'bbs/write.html', {})
     elif request.method == 'POST':
-        return render(request, 'bbs/write.html', {})
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        use_script = (request.POST.get('use_script') == 'on')
+        # TODO: Remove this and use current user from session.
+        bogus_user = BoardUser.objects.all()[0]
+        post = Post(title=title, content=content, allow_script=use_script, author=bogus_user)
+        post.save()
+        return redirect('list')
