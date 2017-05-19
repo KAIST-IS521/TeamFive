35b0db354f27d4267fea91d2d3368ca7a72f5e37
Donghwan Kwon <prious@kaist.ac.kr>
2017-05-20 04:58:02 +0900

    Add dependency install bash file

---
+++ bot/install-deps.bash
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+msg () {
+    echo -e "\033[1;32m$1\033[0;m"
+}
+
+doit () {
+    msg "$*"
+    $*
+}
+
+doit sudo apt-get install -y firefox
+
+if [ ! -f /usr/local/bin/geckodriver ]; then
+    doit sudo chmod +x driver/geckodriver
+    doit sudo cp -f driver/geckodriver /usr/local/bin/geckodriver
+fi
