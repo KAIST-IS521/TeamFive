5fddcf9d35c382d27bfcf81ca761fa3c756e3250
Donghwan Kwon <prious@kaist.ac.kr>
2017-05-20 01:28:32 +0900

    Read a flag from a file, /tmp/IS521GovFlag

--- bot/bot.py
+++ bot/bot.py
@@ -7,18 +7,22 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 def read_posting(driver):
+    # Get post's links by inspecting html
     links = driver.find_elements_by_xpath('/html/body/div/table/tbody/tr/td/a')
     link_num = len(links)
 
     if(link_num == 0):
-        print("Caanot read any post.")
-    else: 
+        print "Caanot read any post."
+
+    else:
+        # Read each post and go back to bbs list page 
         for i in range(len(links)):
             links[i].click()
             WebDriverWait(driver, 5).until(EC.title_contains('Read'))
             driver.execute_script('window.history.go(-1)')
 
 def bbs_login(driver):
+    # Login with admin user for TEST
     login_id = driver.find_element_by_id('username')
     login_id.send_keys('kwon')
 
@@ -30,20 +34,26 @@ def bbs_login(driver):
     try:
         element = WebDriverWait(driver, 5).until(EC.title_contains('list'))
     except:
-        print("Cannot login")
-        #driver.close()
+        print "Cannot login"
     finally:
-        print("Login finished")
+        print "Login finished"
    
 
 def set_cookie(driver):
-    rand_str = lambda n: ''.join([random.choice(string.lowercase) for i in range(n)])
-    flag = rand_str(32)
-    domain = '.bank.com'
+    #rand_str = lambda n: ''.join([random.choice(string.lowercase) for i in range(n)])
+    # Get a flag from the file which a flag updater is generated
+    f = open('/tmp/IS521GovFlag', 'r')
+    flag = f.read()
+    f.close()
+
+    if len(flag) != 16:
+        print "Invalid flag"
+        #driver.close()
+        #return
 
     driver.add_cookie({'name': 'flag', 'value': flag})
-    #driver.add_cookie({'name': 'flag', 'value': flag, 'domain': domain}) 
-    print(flag)
+    #driver.add_cookie({'name': 'flag', 'value': flag, 'domain': '.bank.com'})
+    print "Flag: " + flag
 
 if __name__ == '__main__':
     driver = webdriver.Firefox()
@@ -56,4 +66,4 @@ if __name__ == '__main__':
     bbs_login(driver)
     read_posting(driver)
 
-    driver.close()
+    #driver.close()
