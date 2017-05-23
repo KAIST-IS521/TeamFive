888e30d10c2559c2b427a2bf1826e1ec3cc9c1be
Donghwan Kwon <prious@kaist.ac.kr>
2017-05-23 16:36:16 +0900

    Handle exceptions and fix #30

--- bot/bot.py
+++ bot/bot.py
@@ -1,6 +1,7 @@
 import sys
 import random
 import string
+import time
 from selenium import webdriver
 from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
 from selenium.webdriver.support.ui import WebDriverWait
@@ -11,6 +12,9 @@ FLAG_FILE = '/tmp/IS521GovFlag'
 #Below two variables are temporary for test
 ADMIN_ID = 'kwon'
 ADMIN_PW = 'kwon1234'
+SITE = "127.0.0.1:12345"
+DOMAIN_NAME = "bank.com"
+LOGIN_URI = '/bbs/login/'
 
 def read_posting(driver):
     # Get post's links by inspecting html
@@ -23,12 +27,20 @@ def read_posting(driver):
     else:
         # Read each post and go back to bbs list page 
         for i in range(len(links)):
-            links[i].click()
-            WebDriverWait(driver, 5).until(EC.title_contains('Read'))
-            driver.execute_script('window.history.go(-1)')
-
+            try:
+                WebDriverWait(driver, 5).until(EC.title_contains('list'))
+                links[i].click()
+                WebDriverWait(driver, 5).until(EC.title_contains('Read'))
+                # Wait 1 second after closing all alert boxes
+                while WebDriverWait(driver, 1).until(EC.alert_is_present()):
+                    alert = driver.switch_to_alert()
+                    alert.accept()
+            except:
+                driver.execute_script('window.history.go(-1)')
+ 
 
 def bbs_login(driver):
+    visit_website(driver, SITE + LOGIN_URI)
     #TODO: Change login procee. Login with admin user for TEST
     login_id = driver.find_element_by_id('username')
     login_id.send_keys(ADMIN_ID)
@@ -41,28 +53,35 @@ def bbs_login(driver):
     try:
         element = WebDriverWait(driver, 5).until(EC.title_contains('list'))
     except:
-        print "Cannot login"
-    finally:
-        print "Login finished"
-   
-
-def set_cookie(driver):
-    # Get a flag from the file which a flag updater is generated
-    #TODO: Error handling 
-    f = open(FLAG_FILE, 'r')
-    flag = f.read()
-    f.close()
-
-    # Check flag size
-    if len(flag) != 16:
-        print "Invalid flag"
+        print "[Fail] Cannot login"
         driver.close()
         exit(1)
-    
-    #TODO: Set cross domain cookie
-    driver.add_cookie({'name': 'flag', 'value': flag})
-    #driver.add_cookie({'name': 'flag', 'value': flag, 'domain': 'bank.com'})
-    print "Flag: " + flag
+   
+
+def set_cookie(driver, domain):
+    # Get a flag from the file which a flag updater is generated
+    try:
+        f = open(FLAG_FILE, 'r')
+        flag = f.readline()
+        f.close()
+    except:
+        print "[Fail] Cannot read a flag file, " + FLAG_FILE
+        driver.close()
+        exit(1)
+
+    # Check flag size
+    flag = flag.strip()
+    if len(flag) != 16:
+        print "[Fail] Invalid length of the flag value, %d" % len(flag)
+        driver.close()
+        exit(1)
+
+    # Set a cookie with the flag value
+    try:   
+        driver.add_cookie({'name': 'flag', 'value': flag, 'domain': domain})
+        print "Set-cookie: Flag=" + flag
+    except:
+        print "Cannot set a cookie."
 
 
 def get_url(argv):
@@ -70,23 +89,30 @@ def get_url(argv):
         print "Usage: " + argv[0] + " [goverment homepage URL]:[PORT]"
         #exit(1)
         #TODO: Uncomment an above line and Remove a below line which are for test
-        return 'http://127.0.0.1:12345/bbs/login/'
+        return '127.0.0.1:12345/bbs/login/'
     else:
         return argv[1]
 
+def visit_website(driver, domain):
+    driver.get('http://' + domain)
+    time.sleep(2)
+
+
+def set_driver(driver):
+    driver.implicitly_wait(3)
+    driver.set_window_size(1000, 600)
+   
 
 if __name__ == '__main__':
     site = get_url(sys.argv)
 
-    profile = FirefoxProfile()
-    #profile.set_preference('security.fileuri.strict_origin_policy', False)
+    driver = webdriver.Firefox()
+    set_driver(driver)
 
-    driver = webdriver.Firefox(profile)
-    driver.implicitly_wait(3)
+    visit_website(driver, DOMAIN_NAME)
+    set_cookie(driver, DOMAIN_NAME)
 
-    driver.get(site)
-    set_cookie(driver)
-
+    visit_website(driver, SITE)
     bbs_login(driver)
     read_posting(driver)
 
