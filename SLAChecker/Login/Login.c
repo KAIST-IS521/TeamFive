3f90f81631673e67dd4cd71f5782e9f9d7aa4ad6
bjgwak <bjgwak@kaist.ac.kr>
2017-05-24 00:46:09 +0900

    Insert Initial login checker code

--- SLAChecker/Login/Login.c
+++ SLAChecker/Login/Login.c
@@ -44,7 +44,10 @@ int main(int argc, char** argv){
 
 		//parse_csv(id,password)
 
-		if(login() < 0){	//login(id, password)
+		char* id;
+		char* password;
+
+		if(login(id, password) < 0){	//login(id, password)
 			printf("Login error\n");
 			return 1;
 		}
@@ -57,9 +60,69 @@ int main(int argc, char** argv){
 
 }
 
-
 int connecturl(char* ip, int port){
 
+	CURL *curl;
+
+	CURLcode res;
+
+	struct recv_data data;
+
+	struct curl_slist *cookies;
+	
+
+	data.data = malloc(1);
+	data.size = 0;
+
+	curl = curl_easy_init();
+
+	if(curl){
+
+		curl_easy_setopt(curl, CURLOPT_URL, "127.0.0.1:12345/bbs/");
+		//curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteMemoryCallback);
+		//curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void*) &data);
+		curl_easy_setopt(curl, CURLOPT_COOKIEFILE, "-");
+
+		res = curl_easy_perform(curl);
+		
+		if(res != CURLE_OK) 
+			{
+				fprintf(stderr, "curl,easy_perform() failed: %s\n", curl_easy_strerror(res));
+
+			
+				return -1;
+			}
+		else{
+
+			printf("Enter the bbs page successfully\n");
+
+			res = curl_easy_getinfo(curl, CURLINFO_COOKIELIST, &cookies);
+
+		/*curl_easy_setopt(curl, CURLOPT_URL, "127.0.0.1:12345/bbs/login/");
+		curl_easy_setopt(curl, CURLOPT_POSTFIELDS, "username=bjgwak&password=bbbbbbbb&csrfmiddlewaretoken=CSRF_TOKEN");
+		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteMemoryCallback);
+		curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void*) &data);*/
+		if(!res && cookies){
+			while(cookies){
+				printf("%s\n", cookies->data);
+				}
+		}
+
+		//res = curl_easy_perform(curl);
+
+//printf("%s\n", data.data);
+			
+			return -1;
+
+		}
+
+
+	}
+
+}
+
+int login(char* id, char* password){
+	
 	CURL *curl;
 
 	CURLcode res;
@@ -72,7 +135,8 @@ int connecturl(char* ip, int port){
 
 	if(curl){
 
-		curl_easy_setopt(curl, CURLOPT_URL, "127.0.0.1:12345/bbs/");
+		curl_easy_setopt(curl, CURLOPT_URL, "127.0.0.1:12345/bbs/login/");
+		curl_easy_setopt(curl, CURLOPT_POSTFIELDS, "username=bjgwak&password=bbbbbbbb");
 		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteMemoryCallback);
 		curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void*) &data);
 
@@ -88,7 +152,7 @@ int connecturl(char* ip, int port){
 			}
 		else{
 
-			printf("Enter the bbs page successfully\n");
+			printf("%s\n", data.data);
 
 			
 			return 0;
@@ -97,12 +161,5 @@ int connecturl(char* ip, int port){
 
 
 	}
-
-}
-
-int login(char* ip, int port){
-
-
-	return 0;
 }
 
