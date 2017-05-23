7e4e26868c55ce606d38e399d402ca037f61bcb9
bjgwak <bjgwak@kaist.ac.kr>
2017-05-23 20:36:41 +0900

    Insert initial curl code

--- SLAChecker/Login.c
+++ SLAChecker/Login.c
@@ -1,5 +1,31 @@
 #include <stdio.h>
 #include <curl/curl.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct recv_data{
+	char * data;
+	size_t size;
+};
+
+static size_t WriteMemoryCallback(void *contents, size_t size, size_t nmemb, void *userp)
+{
+  size_t realsize = size * nmemb;
+  struct recv_data *data = (struct recv_data *)userp;
+
+  data->data = realloc(data->data, data->size + realsize + 1);
+  
+  if(data->data == NULL) {
+    printf("not enough memory (realloc returned NULL)\n");
+    return 0;
+  }
+  
+  memcpy(&(data->data[data->size]), contents, realsize);
+  data->size += realsize;
+  data->data[data->size] = 0;
+
+  return realsize;
+}
 
 int main(int argc, char** argv){
 
@@ -11,7 +37,7 @@ int main(int argc, char** argv){
 	char* ip = argv[1];
 	int port = atoi(argv[2]);
 
-	if(connect(ip, port) < 0){
+	if(connecturl(ip, port) < 0){
 		return 2;
 	}
 	else{
@@ -28,8 +54,38 @@ int main(int argc, char** argv){
 }
 
 
-int connect(char* ip, int port){
+int connecturl(char* ip, int port){
 
+	CURL *curl;
+
+	CURLcode res;
+
+	struct recv_data data;
+	data.data = malloc(1);
+	data.size = 0;
+
+	curl = curl_easy_init();
+
+	if(curl){
+
+		curl_easy_setopt(curl, CURLOPT_URL, "http://www.example.com/");
+		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteMemoryCallback);
+		curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void*) &data);
+
+
+		res = curl_easy_perform(curl);
+		
+		if(res != CURLE_OK) 
+			fprintf(stderr, "curl,easy_perform() failed: %s\n", curl_easy_strerror(res));
+		else{
+
+			printf("%lu\n", data.size);
+
+		}
+
+
+		curl_easy_cleanup(curl);
+	}
 
 	return -1;
 }
