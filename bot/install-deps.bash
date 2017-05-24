d533142c2977d4c006d1408261fc7a217421a9d7
Yunjong Jeong <yunjong@kaist.ac.kr>
2017-05-24 22:47:59 +0900

    Fetch geckodriver from the web

--- bot/install-deps.bash
+++ bot/install-deps.bash
@@ -13,6 +13,7 @@ doit () {
 doit sudo apt-get install -y firefox-esr xvfb
 
 if [ ! -f /usr/local/bin/geckodriver ]; then
-    doit sudo chmod +x driver/geckodriver
-    doit sudo cp -f driver/geckodriver /usr/local/bin/geckodriver
+    doit curl -Lo /tmp/gecko.tar.gz https://github.com/mozilla/geckodriver/releases/download/v0.14.0/geckodriver-v0.14.0-linux32.tar.gz
+    doit cd /tmp && tar xf /tmp/gecko.tar.gz
+    doit sudo mv /tmp/geckodriver /usr/local/bin/geckodriver
 fi
