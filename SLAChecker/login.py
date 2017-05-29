935d47fcc73a544fa0d6b9c75a6fb2ea6f99aa73
bjgwak <bjgwak@kaist.ac.kr>
2017-05-29 10:45:41 +0900

    Modify login process to handle the wrong case

--- SLAChecker/login.py
+++ SLAChecker/login.py
@@ -2,14 +2,15 @@ import os
 import sys
 import requests
 import gnupg
+import csv
 from common import ID, PW, get_csrf_token, test_connection
 
 
-def test_login(domain, session):
+def test_login(domain, session, lineid, linepw):
     r = session.get(domain + '/bbs/login')
     token = get_csrf_token(r)
 
-    payload = {'username': ID, 'password': PW, 'csrfmiddlewaretoken': token}
+    payload = {'username': lineid, 'password': linepw, 'csrfmiddlewaretoken': token}
     r = session.post(domain + '/bbs/login/', data = payload )
 
     if r.status_code == requests.codes.ok:
@@ -28,14 +29,29 @@ def test_logout(domain, session):
     return True
 
 def run(domain, session):
-    if test_login(domain, session) and test_logout(domain, session):
-        return 0
-    return 1
+
+    successindex = True;
+
+    f = open('./input.csv', 'r')
+    csvReader = csv.reader(f)
+    
+    for row in csvReader:
+        if test_login(domain, session, row[0].split(' ')[0], row[0].split(' ')[1]) and test_logout(domain, session):
+            continue;
+        else
+            return 1
+    
+    return 0
 
 if __name__ == '__main__':
+
     if len(sys.argv) < 3:
         print("Usage: {} <ip> <port>".format(sys.argv[0]))
         exit(1)
+
+   
+
+
 
     session = requests.Session()
     domain = 'http://{}:{}'.format(sys.argv[1], sys.argv[2])
